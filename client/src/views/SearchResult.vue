a7e294b5caa91b06c33d281c711a3a5701cc2048
Jun <84994911+airweather@users.noreply.github.com>
2022-12-07 04:56:06 +0900

    Update SearchResult.vue

--- client/src/views/SearchResult.vue
+++ client/src/views/SearchResult.vue
@@ -2,15 +2,14 @@
     <main class="mt-5">
       <div class="container bg-white">
         <h3>Result : <b>{{searchResult}}</b></h3>
-          <div class="row g-3">
-              <div class="col-xxl-3 col-xl-4 col-md-6" :key="i" v-for="(look, i) in searchResultList">
-                  <div class="card" style="width: 20rem; cursor: pointer;">
-                      <a @click="toInfoPage(look);"><img :src="`/download/${look.name}/${look.image}`" class="card-img" alt="..."></a>
-                      <p class="card-text"><b>{{look.name}}</b> | {{look.date}}</p>
-                  </div>
-              </div>
-              
+        <div class="row g-3">
+          <div class="col-xxl-3 col-xl-4 col-md-6" :key="i" v-for="(look, i) in searchResultList">
+            <div class="card" style="width: 20rem; cursor: pointer;">
+              <a @click="toInfoPage(look);"><img :src="`/download/${look.name}/${look.image}`" class="card-img" alt="..."></a>
+              <p class="card-text"><b>{{look.name}}</b> | {{look.date}}</p>
+            </div>
           </div>
+        </div>
       </div>
     </main>
   </template>
@@ -38,7 +37,6 @@
      
       let searchResult = await this.$api("/api/searchResult", {param:[this.$route.query.data]})
       this.searchResultList = searchResult
-      console.log('search : ', {id:this.$route.query.data})
     },
       toInfoPage(look) {
         this.$router.push({path:'/info', query:{id:look.id, name:look.name, image:look.image}})
